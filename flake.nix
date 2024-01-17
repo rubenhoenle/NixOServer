eb1164794ce139a17c9e42760918e8452b2904b2
Ruben Hoenle <git@hoenle.xyz>
2024-01-17 17:38:36 +0100

    feat(nixos): fix nixpkgs url in flake
    and make home-manager follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,8 +2,11 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     # agenix for encrypting secrets
