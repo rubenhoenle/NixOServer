d9e62d74ebc969c7d39bdcc970a41c71db88ba20
Ruben Hoenle <git@hoenle.xyz>
2024-11-08 20:46:25 +0100

    chore(gitserver): remove service-specific backup service

--- services/gitserver.nix
+++ services/gitserver.nix
@@ -35,51 +35,6 @@
             X11Forwarding no
         '';
       };
-
-      /* backup service */
-      services.restic.backups.gitserver = {
-        user = "git";
-        initialize = true;
-        passwordFile = config.age.secrets.resticPassword.path;
-        repository = "s3:https://s3.eu-central-003.backblazeb2.com/nixos-server-restic-backup/services/gitserver";
-        environmentFile = config.age.secrets.backblazeB2ResticS3EnvironmentSecrets.path;
-        paths = [
-          "/var/lib/git-server"
-        ];
-        backupPrepareCommand = "${pkgs.curl}/bin/curl -fsS -m 10 --retry 5 https://hc-ping.com/$(${pkgs.coreutils}/bin/cat ${config.age.secrets.healthchecksIoUuid.path})/backup-gitserver/start";
-        pruneOpts = [
-          "--keep-hourly 48"
-          "--keep-daily 7"
-          "--keep-weekly 4"
-          "--keep-monthly 12"
-          "--keep-yearly 3"
-        ];
-        extraOptions = [ "s3.region=eu-central-003" ];
-        timerConfig = {
-          OnCalendar = "hourly";
-          Persistent = true;
-        };
-      };
-
-      systemd.services."restic-backups-gitserver" = {
-        onSuccess = [ "restic-notify-gitserver@success.service" ];
-        onFailure = [ "restic-notify-gitserver@failure.service" ];
-      };
-
-      systemd.services."restic-notify-gitserver@" =
-        let
-          script = pkgs.writeText "restic-notify-gitserver.sh"
-            ''
-              ${pkgs.curl}/bin/curl -fsS -m 10 --retry 5 https://hc-ping.com/$(cat ${config.age.secrets.healthchecksIoUuid.path})/backup-gitserver/''${MONITOR_EXIT_STATUS}
-            '';
-        in
-        {
-          serviceConfig = {
-            Type = "oneshot";
-            User = "git";
-            ExecStart = "${pkgs.bash}/bin/bash ${script}";
-          };
-        };
     };
 }
 
