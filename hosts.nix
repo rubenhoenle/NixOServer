a845b5fe58960eb276d032511dee8f88c018a8d0
Ruben Hoenle <git@hoenle.xyz>
2024-11-13 01:19:38 +0100

    chore(hosts): remove scarif

--- hosts.nix
+++ hosts.nix
@@ -38,30 +38,4 @@
       }
     ];
   }
-  {
-    name = "scarif";
-    system = "aarch64-linux";
-    nixosModules = [
-      ./hardware/raspberry-pi-4.nix
-      {
-        networking.hostName = "scarif";
-
-        ruben.filebrowser.enable = true;
-
-        services.unbound.enable = false;
-
-        boot.loader = {
-          # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-          grub.enable = false;
-          # Enables the generation of /boot/extlinux/extlinux.conf
-          generic-extlinux-compatible.enable = true;
-        };
-
-        system = {
-          autoUpgrade.enable = true;
-          stateVersion = "24.05";
-        };
-      }
-    ];
-  }
 ]
