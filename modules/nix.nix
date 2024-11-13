414ab0c5b537d855c2023006672d223accac0f0d
Ruben Hoenle <git@hoenle.xyz>
2024-11-13 01:20:30 +0100

    chore(nix): remove auto system upgrade options

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,17 +1,4 @@
 {
-  system.autoUpgrade = {
-    flake = "github:rubenhoenle/NixOServer";
-    flags = [ "--accept-flake-config" ];
-    randomizedDelaySec = "1h";
-    allowReboot = true;
-    rebootWindow = {
-      lower = "04:00";
-      upper = "06:00";
-    };
-    /* Note: This must be during the reboot window for the reboot to happen. */
-    dates = "04:00";
-  };
-
   nix = {
     # nix garbage collection
     gc = {
