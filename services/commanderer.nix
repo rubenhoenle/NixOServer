1fb61ecf46c082eacbbdf32e36e743c635da4d52
Ruben Hoenle <git@hoenle.xyz>
2025-05-10 16:41:03 +0200

    feat(commanderer): reverse proxy setup

--- services/commanderer.nix
+++ services/commanderer.nix
@@ -24,7 +24,8 @@ in
         stopIfChanged = false;
         startLimitIntervalSec = 60;
         environment = {
-          COMMANDERER_LISTEN_HOST = "0.0.0.0";
+          #COMMANDERER_LISTEN_HOST = "0.0.0.0";
+          COMMANDERER_LISTEN_HOST = "127.0.0.1";
         };
         serviceConfig = {
           ExecStart = "${commanderer}/bin/commanderer";
@@ -36,6 +37,20 @@ in
       };
 
       /* open firewall ports */
-      networking.firewall.allowedTCPPorts = [ 9000 ];
+      #networking.firewall.allowedTCPPorts = [ 9000 ];
+
+      /* reverse proxy configuration */
+      services.nginx.virtualHosts."commanderer.hoenle.xyz" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:9000";
+          extraConfig =
+            # required when the server wants to use HTTP Authentication
+            #"proxy_pass_header Authorization;" +
+            "client_max_body_size 200M;"
+          ;
+        };
+      };
     };
 }
